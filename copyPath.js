cf8ee2c92e64f3e5093d49b9109d8da518c69a93
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-09-05 14:40:34 +0900

    published

--- copyPath.js
+++ copyPath.js
@@ -33,24 +33,24 @@ String.prototype.filterFileName = function() {
 
 /**
  * @name 경로 복사
- * @param {string || array} name
  * @param {string} path
  * @param {string} savePath
+ * @param {string || array} saveName
  * @since 2018-09-05
  */
-function copyPath(name, path, savePath) {
+function copyPath(path, savePath, saveName) {
 	//문자이면서 값이 있을때
-	if(typeof name === 'string' && name) {
-		name = [name];
+	if(typeof saveName === 'string' && saveName) {
+		saveName = [saveName];
 	}
 
-	//배열일때
-	if(Array.isArray(name)) {
+	//문자일때
+	if(typeof path === 'string') {
 		//문자일때
-		if(typeof path === 'string') {
-			//문자일때
-			if(typeof savePath === 'string') {
-				name.forEach((value, index, array) => {
+		if(typeof savePath === 'string') {
+			//배열일때
+			if(Array.isArray(saveName)) {
+				saveName.forEach((value, index, array) => {
 					value = savePath + value.filterFileName();
 
 					try {
@@ -61,42 +61,40 @@ function copyPath(name, path, savePath) {
 					}
 				});
 			}else{
-				console.error('savePath : 문자가 아닙니다.');
+				console.error('saveName : 배열이 아닙니다.');
 			}
 		}else{
-			console.error('path : 문자가 아닙니다.');
+			console.error('savePath : 문자가 아닙니다.');
 		}
 	}else{
-		console.error('name : 배열이 아닙니다.');
+		console.error('path : 문자가 아닙니다.');
 	}
 }
 
 //질문
-rl.question('이름 : ', (name) => {
+rl.question('경로 : ', (path) => {
 	//값이 있을때
-	if(name) {
-		name = name.removeBlank().split(',');
-
-		rl.question('경로 : ', (path) => {
+	if(path) {
+		rl.question('저장경로 : ', (savePath) => {
 			//값이 있을때
-			if(path) {
-				rl.question('저장경로 : ', (savePath) => {
+			if(savePath) {
+				rl.question('저장이름 : ', (saveName) => {
 					//값이 있을때
-					if(savePath) {
-						copyPath(name, path, savePath);
+					if(saveName) {
+						copyPath(path, savePath, saveName.removeBlank().split(','));
 					}else{
-						console.error('저장경로를 입력해주세요.');
+						console.error('저장이름을 입력해주세요.');
 					}
 
-					rl.close();	
+					rl.close();
 				});
 			}else{
-				console.error('경로를 입력해주세요.');
-				rl.close();
+				console.error('저장경로를 입력해주세요.');
+				rl.close();	
 			}
 		});
 	}else{
-		console.error('이름을 입력해주세요.');
+		console.error('경로를 입력해주세요.');
 		rl.close();
 	}
 });
