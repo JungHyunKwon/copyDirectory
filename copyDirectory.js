da1aed0dd350ebb50298b87c0cbe97c78bb7c66b
JungHyunKwon <kjh_3859@naver.com>
2019-06-10 17:26:52 +0900

    published

--- copyDirectory.js
+++ copyDirectory.js
@@ -9,7 +9,6 @@
 const fs = require('fs-extra'), // {@link https://github.com/jprichardson/node-fs-extra}
 	  filenamify = require('filenamify'), // {@link https://github.com/sindresorhus/filenamify}
 	  readline = require('readline'),
-	  isArray = Array.isArray,
 	  rl = readline.createInterface({
 	      input : process.stdin,
 		  output : process.stdout
@@ -21,102 +20,86 @@ const fs = require('fs-extra'), // {@link https://github.com/jprichardson/node-f
 	   saveDirectory : string,
 	   names : array
    }
-   @param {function} callback {
-       isLast : boolean,
-	   isSaved : boolean,
-	   saveDirectory : string
-   }
+   @param {function} callback {array}
  * @since 2018-09-05
  */
 function copyDirectory(options, callback) {
-	let directory = options.directory;
+	let callbackIsFunction = typeof callback === 'function',
+		result = [];
 
-	//문자일 때
-	if(typeof directory === 'string') {
-		let saveDirectory = options.saveDirectory;
+	//객체일 때
+	if(options) {
+		let names = options.names;
+		
+		//배열일 때
+		if(Array.isArray(names)) {
+			let directory = options.directory,
+				saveDirectory = options.saveDirectory,
+				namesLength = names.length;
 
-		//문자일 때
-		if(typeof saveDirectory === 'string') {
-			let names = options.names;
+			(function loopNames(index) {
+				//이름 개수만큼 반복
+				if(namesLength > index) {
+					let nameDirectory = saveDirectory + '/' + filenamify(names[index], {
+						replacement : ''
+					});
 
-			//배열일 때
-			if(isArray(names)) {
+					fs.copy(directory, nameDirectory, err => {
+						result.push({
+							directory : directory,
+							saveDirectory : nameDirectory,
+							isSaved : (err) ? false : true //오류가 있을 때
+						});
+
+						loopNames(index + 1);
+					});
+
 				//함수일 때
-				if(typeof callback === 'function') {
-					let namesLength = names.length;
-
-					(function loopNames(index) {
-						let callbackOptions = {
-							isSaved : false,
-							isLast : false,
-							saveDirectory : ''
-						};
-
-						//이름 개수만큼 반복
-						if(namesLength > index) {
-							callbackOptions.saveDirectory = saveDirectory + '/' + filenamify(names[index], {
-								replacement : ''
-							});
-
-							fs.copy(directory, callbackOptions.saveDirectory, (err) => {
-								//오류가 없을 때
-								if(!err) {
-									callbackOptions.isSaved = true;
-								}
-
-								callback(callbackOptions);
-
-								loopNames(index + 1);
-							});
-						}else{
-							callbackOptions.isLast = true;
-
-							callback(callbackOptions);
-						}
-					})(0);
-				}else{
-					console.error('callback : 함수가 아닙니다.');
+				}else if(callbackIsFunction) {
+					callback(result);
 				}
-			}else{
-				console.error('names : 배열이 아닙니다.');
-			}
-		}else{
-			console.error('saveDirectory : 문자가 아닙니다.');
+			})(0);
+
+		//함수일 때
+		}else if(callbackIsFunction) {
+			callback(result);
 		}
-	}else{
-		console.error('directory : 문자가 아닙니다.');
+	
+	//함수일 때
+	}else if(callbackIsFunction) {
+		callback(result);
 	}
 }
 
 //질문
-rl.question('경로 : ', (directory) => {
+rl.question('경로 : ', directory => {
 	//값이 있을 때
 	if(directory) {
-		rl.question('저장 경로 : ', (saveDirectory) => {
+		rl.question('저장 경로 : ', saveDirectory => {
 			//값이 있을 때
 			if(saveDirectory) {
 				console.log('\n쉼표로 구분할 수 있습니다.\n');
 
-				rl.question('이름 : ', (names) => {
+				rl.question('이름 : ', names => {
 					//값이 있을 때
 					if(names) {
 						copyDirectory({
 							directory : directory,
 							saveDirectory : saveDirectory,
 							names : names.split(',')
-						}, (result) => {
-							let saveDir = result.saveDirectory;
+						}, result => {
+							result.forEach((value, index, arraay) => {
+								let nameDirectory = value.saveDirectory;
+
+								//저장했을 때
+								if(value.isSaved) {
+									console.log(nameDirectory + '에 복사하였습니다.');
+								}else{
+									console.error(nameDirectory + '에 복사하지 못했습니다.');
+								}
+							});
 							
-							//마지막일 때
-							if(result.isLast) {
-								console.log('작업을 완료하였습니다.');
-							
-							//저장했을 때
-							}else if(result.isSaved) {
-								console.log(saveDir + '에 복사하였습니다.');
-							}else{
-								console.error(saveDir + '에 복사 실패하였습니다.');
-							}
+							console.log('작업을 완료하였습니다.');
 						});
 					}else{
 						console.error('이름을 입력해주세요');
